c34f67e8002d0fea3a0a5456cdad1d0ef2db257e
sethgerou <seth.gerou@gmail.com>
2017-01-25 15:21:03 -0800

    switched case to if/else for rubric points

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,6 +1,7 @@
 # Ask how many times to execute the loop
 puts "how many employees need to be processed?"
 to_process = gets.chomp
+
 counter = 1
 
 # iterates through counter until 'to_process' is reached.
@@ -37,20 +38,19 @@ while counter <= to_process.to_i
     end
   end
 
-  case
-  when name == "Drake Cula", name == "Tu Fang"
-    decision = " are definitely a vampire"
-  when allergy.downcase == "sunshine"
-    decision = "are probably a vampire"
-  when (age.to_i != Time.now.year - birth_year.to_i || age.to_i != (Time.now.year - birth_year.to_i)-1) && (garlic_response.downcase == "no") && (insurance_choice.downcase == "no")
-        # Note: we don't know what month the employee was born in, so we're allowing for the possibility they haven't had their birthday yet this year.
-    decision = " are almost certainly a vampire"
-  when (age.to_i != Time.now.year - birth_year.to_i || age.to_i != (Time.now.year - birth_year.to_i)-1) && (garlic_response.downcase == "no" || insurance_choice.downcase == "no")
-    decision = " are probably a vampire"
-  when (age.to_i == Time.now.year - birth_year.to_i || age.to_i == (Time.now.year - birth_year.to_i)-1) && (garlic_response.downcase == "yes" || insurance_choice.downcase == "yes")
-    decision = " are probably not a vampire"
-  else
-    decision = "'re results are inconclusive"
+  if name == "Drake Cula" or name == "Tu Fang"
+      decision = " are definitely a vampire"
+    elsif allergy.downcase == "sunshine"
+      decision = "are probably a vampire"
+    elsif (age.to_i != Time.now.year - birth_year.to_i || age.to_i != (Time.now.year - birth_year.to_i)-1) && (garlic_response.downcase == "no") && (insurance_choice.downcase == "no")
+          # Note: we don't know what month the employee was born in, so we're allowing for the possibility they haven't had their birthday yet this year.
+      decision = " are almost certainly a vampire"
+    elsif (age.to_i != Time.now.year - birth_year.to_i || age.to_i != (Time.now.year - birth_year.to_i)-1) && (garlic_response.downcase == "no" || insurance_choice.downcase == "no")
+      decision = " are probably a vampire"
+    elsif (age.to_i == Time.now.year - birth_year.to_i || age.to_i == (Time.now.year - birth_year.to_i)-1) && (garlic_response.downcase == "yes" || insurance_choice.downcase == "yes")
+      decision = " are probably not a vampire"
+    else
+      decision = "'re results are inconclusive"
   end
 
   puts "Employee #{counter}, We've decided you#{decision}.\n\n\n"
