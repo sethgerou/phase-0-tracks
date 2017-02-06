ccff5df4059e5511d4548e27ce820d9d778e84d5
sethgerou <seth.gerou@gmail.com>
2017-02-06 15:16:27 -0800

    add code to define Puppy class, comment functionality, and create a few instances.

---
+++ ruby/puppy.rb
@@ -0,0 +1,28 @@
+# puppy profiler
+
+class Puppy
+
+=begin
+    species: canis familiaris
+
+    characteristics:
+      color: brown
+      markings: white feet
+      coat length: short
+      eye color: brown
+      tail length: long
+
+    behavior:
+      temperment: calm
+      good with other dogs? yes
+      good with children? yes
+      housebroken? no
+      commands known: sit
+=end
+
+end
+
+rex = Puppy.new
+dixie = Puppy.new
+barksalot = Pupppy.new
+bobbarker = Puppy.new
