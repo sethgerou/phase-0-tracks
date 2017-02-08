c93e4568ee669f3b55c15f37ae4bec900afb4ad9
sethgerou <seth.gerou@gmail.com>
2017-02-08 15:29:57 -0800

    add code to require SecureRandom to generate a unique id for the politician class.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,5 @@
+require 'SecureRandom' # adding 'SecoreRandom' to generate a uuid
+
 # new shout module with two mixin classes
 module Shout
   def angrily(phrase)
@@ -6,6 +8,10 @@ module Shout
 
   def happily(phrase)
     puts "#{phrase}".downcase + ":) did I say that out loud?"
+  end
+
+  def uuid # method to generate a uuid
+    SecureRandom.uuid
   end
 end
 
@@ -24,6 +30,9 @@ person.angrily("I wish it would stop snowing")
 politician  = Politician.new
 politician.happily("I can't believe my constituents let me get away with this.  ")
 
+pol_id = politician.uuid # calling a pre-existing module
+puts "This politician's Unique id is #{pol_id}.  Don't let him get away with this!"
+
 =begin - old Shout module code
 module Shout
   def self.yell_angrily(words)
@@ -34,6 +43,8 @@ module Shout
     words.upcase + "! <B <3 <B :) !"
   end
 end
+
+# old driver code
 
 puts Shout.yell_angrily("Hey, you there").capitalize
 puts Shout.yelling_happily("I'm so glad the snow is finally melting")
