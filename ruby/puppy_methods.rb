a609bfbbdac17feef142e5204fac8bb488381990
sethgerou <seth.gerou@gmail.com>
2017-02-08 10:33:18 -0800

    saved pairs code - when switching roles

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,35 @@
 class Puppy
 
-  def fetch(toy)
+    def initialize
+        puts "Initializing new puppy instance..."
+    end
+
+ def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
 
+ def speak(number)
+      number.times{ puts "Woof!"}
+  end
+      
+  def roll_over
+          puts "*rolls over*"
+  end
+
+    def dog_years(years)
+     years * 7
+    end
+
+    def sit
+        puts "*Puppy sits*"
+    end
+
 end
 
+fido = Puppy.new
+fido.fetch("ball")
+fido.speak(2)
+fido.roll_over
+puts fido.dog_years(3)
+fido.sit
