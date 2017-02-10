eae1d9d1256db942a64224b33ea25fda32a39859
sethgerou <seth.gerou@gmail.com>
2017-02-09 18:23:35 -0800

    method code to implement game functionality. also driver code.  I know, I know, I should have done many many commits - I got so engrossed in figuring out how to build tests in rspec, and how to get the game to work, I completely forgot.

--- ruby/wordgame/wordgame_spec.rb
+++ ruby/wordgame/wordgame_spec.rb
@@ -1,17 +1,36 @@
 require_relative 'wordgame'
 describe Wordgame do
-  let(:game) {Wordgame.new}
+  let(:game) {Wordgame.new("userword")}
 
-it "" do
-  expect
-end
+  it "stores player 1's word" do
+    expect(game.get_word).to eq ["u","s","e","r","w","o","r","d"]
+  end
 
-it "" do
-  expect
-end
+  it "evaluates whether guess is in word" do
+    expect(game.guess("u")).to be true
+  end
 
-it "" do
-  expect
-end
+  it "stores guess in an array of guesses" do
+    game.guess("u")
+    expect(game.guessed.include?("u")).to be true
+  end
 
+  it "adds guess to word status" do
+    game.guess("w")
+    expect(game.word_status).to eq ["-","-","-","-","w","-","-","-"]
+  end
+
+  it "adjusts the count of available guesses" do
+    game.count_minus
+    expect(game.guess_count).to eq 15
+  end
+
+  it "displays the current word status" do
+    expect(game.word_status).to eq ["-","-","-","-","-","-","-","-"]
+  end
+
+
+  it "generates the end message" do
+    expect(game.end_message("lose")).to eq "Sorry, keep trying kid."
+  end
 end
