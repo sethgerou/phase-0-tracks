28d89326e444be6307c8f3b9e753c5c1d41d20ae
sethgerou <seth.gerou@gmail.com>
2017-02-11 09:28:03 -0800

    pseudocode for grocery list

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,29 @@
+# GPS2.2
+
+## Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps:
+  # create a hash
+  # set default quantity (1)
+  # print the list to the console - method to print the list
+# output: return the hash
+
+# Method to add an item to a list
+# input: list, item name, and optional quantity
+# steps: add item name => quanitity to hash
+# output: print list
+
+# Method to remove an item from the list
+# input: list, item name
+# steps: delete item name from list hash
+# output: print list
+
+# Method to update the quantity of an item
+# input: list, item name, quanity
+# steps: update the value of the the key item name in list hash
+# output: item name quanity updated
+
+# Method to PRINT A LIST and make it look pretty
+# input: list (hash)
+# steps: for each item/quanity print a line like Item: quantity
+# output: print the hash
