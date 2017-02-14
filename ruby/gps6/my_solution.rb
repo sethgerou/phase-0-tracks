74fca3d9d3686bf98a34d81953be752b8b3702d9
sethgerou <seth.gerou@gmail.com>
2017-02-13 18:00:49 -0800

    finished refactoring and added reflection comments.

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -1,5 +1,5 @@
-# I worked on this challenge [by myself, with: ].
-# We spent [#] hours on this challenge.
+# I worked on this challenge [by myself, with: Josh Mun ].
+# We spent [1.5] hours on this challenge.
 
 # EXPLANATION OF require_relative
 # The require method will pull code from another file and sort of copy and paste that code into this working file.
@@ -18,8 +18,6 @@ class VirusPredictor
     @population_density = population_density
   end
 
-
-
 # Calls 2 methods
   def virus_effects
     predicted_deaths
@@ -33,16 +31,15 @@ class VirusPredictor
  def predicted_deaths
     # predicted deaths is solely based on population density
 
-    densities = {200 => 0.4, 150 => 0.3, 100 => 0.2, 50 => 0.1, 0 => 0.05}
-    # density = {0 => 0.05, 50 => 0.1, 100 => 0.2, 150 => 0.3, 200 => 0.4}
+    densities = {0 => 0.05, 50 => 0.1, 100 => 0.2, 150 => 0.3, 200 => 0.4}
 
     densities.each do |density, ratio|
        if @population_density >= density
-         number_of_deaths = (@population * ratio).floor
+         @number_of_deaths = (@population * ratio).floor
        end
      end
 
-=begin
+=begin - refactored
     if @population_density >= 200
       number_of_deaths = (@population * 0.4).floor
     elsif @population_density >= 150
@@ -56,7 +53,7 @@ class VirusPredictor
     end
 =end
 
-    print "#{@state} will lose #{number_of_deaths} people in this outbreak"
+    print "#{@state} will lose #{@number_of_deaths} people in this outbreak"
 
  end
 
@@ -67,6 +64,16 @@ class VirusPredictor
     # by additional factors we haven't added into this functionality.
     speed = 0.0
 
+    # spread_rates = {200 => 0.5, 150 => 1, 100 => 1.5, 50 => 2, 0 => 2.5}
+    spread_rates = {0 => 2.5, 50 => 2, 100 => 1.5, 150 => 1, 200 => 0.5}
+
+    spread_rates.each do |density, rate|
+       if @population_density >= density
+         speed = rate
+       end
+     end
+
+=begin - refactored
    if @population_density >= 200
       speed += 0.5
     elsif @population_density >= 150
@@ -78,6 +85,7 @@ class VirusPredictor
     else
       speed += 2.5
     end
+=end
 
    puts " and will spread across the state in #{speed} months.\n\n"
 
@@ -112,3 +120,24 @@ end
 
 #=======================================================================
 # Reflection Section
+
+=begin
+What are the differences between the two different hash syntaxes shown in the state_data file?
+
+- In terms of the hash, there is no difference.  writing :key => value is exactly the same as writing key: value - the hash-rocket notation is used for "state" => {key} in STATE_DATA as the state name is written as a string, not a symbol.
+
+What does require_relative do? How is it different from require?
+- Require_relative imports an external file containing code or modules.
+require imports code from installed gems - such as loading SecureRandom to use its methods like uuid.
+
+What are some ways to iterate through a hash?
+- By key, by value, by key and value
+
+When refactoring virus_effects, what stood out to you about the variables, if anything?
+a variable initialized within an each do iteration won't be available oustide the loop.  It either needs to be an instance @variable, or declared outside (before) the loop like variable = 0.
+
+What concept did you most solidify in this challenge?
+- Got a nice refresher in nested data structures.  
+- Also got a better sense of the utility of an instance variable.
+
+=end
