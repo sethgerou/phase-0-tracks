2f073f28b70dfdbac6e2d2f18f8b88c3197c5141
sethgerou <seth.gerou@gmail.com>
2017-02-15 14:36:49 -0800

    commented code

--- js/explore.js
+++ js/explore.js
@@ -3,16 +3,18 @@
 // output: reversed string - like 'gnirts'
 
 function reverse(string) {
+  // a function to reverse the characters in the arguement (a string)
   var new_str = "";
     for (var i = string.length; i > 0; i-=1) {
+      // for the number of characters in the string, take one character at a time using string[index "i"-1] and add it to new_str.
       new_str = new_str + string[i-1];
     }
-  return new_str;
+  return new_str; // return the fully reversed string
 }
 
 // driver code
-var backwards = reverse("octopus");
+var backwards = reverse("octopus");  //reverse the string "octopus"
 
-if ("apple" != "orange") {
+if ("apple" != "orange") {  // if 'apple' is not equal to 'orange' print the reversed string.
   console.log(backwards);
 }
