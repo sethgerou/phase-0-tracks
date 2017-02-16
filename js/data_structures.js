79d0dbcca6fe69bf9ed68e26aabeccb2318f814e
sethgerou <seth.gerou@gmail.com>
2017-02-16 13:32:40 -0800

    add function to assign colors to horses

--- js/data_structures.js
+++ js/data_structures.js
@@ -6,3 +6,16 @@ names.push("Trusty");
 
 console.log(colors);
 console.log(names);
+
+// function to assign name and color to a new object
+function assignColors(ary1, ary2) {
+  horses = {}
+    if (ary1.length == ary2.length) {
+      for (var n = 0; n < ary1.length; n++) {
+        horses[ary1[n]] = ary2[n];
+      }
+  }
+  return horses;
+};
+
+console.log(assignColors(names, colors));
